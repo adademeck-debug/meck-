da18cd9893d52308463769d61097fcf4a0b44f09
Meck Adade Smith <uglygod@Mecks-MacBook-Pro.local>
2026-04-19 00:53:51 +0100

    add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://greenlinefacilities.co.uk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://greenlinefacilities.co.uk/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://greenlinefacilities.co.uk/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
